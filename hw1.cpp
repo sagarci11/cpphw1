a9f987f4504ed8d32932ec023b6ab4b9294e60b7
greenprotein <garciasa@onid.oregonstate.edu>
2018-08-29 00:47:20 -1000

    hi?

---
+++ hw1.cpp
@@ -0,0 +1,8 @@
+//
+//  hw1.cpp
+//  
+//
+//  Created by Samantha Garcia on 8/29/18.
+//
+
+#include "hw1.hpp"
---
+++ hw1.hpp
@@ -0,0 +1,13 @@
+//
+//  hw1.hpp
+//  
+//
+//  Created by Samantha Garcia on 8/29/18.
+//
+
+#ifndef hw1_hpp
+#define hw1_hpp
+
+#include <stdio.h>
+
+#endif /* hw1_hpp */
